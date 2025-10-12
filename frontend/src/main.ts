3970f80a9d704597cac801bba91a52b356407241
mortal <wuchen.cg@gmail.com>
2025-10-13 00:21:13 +0800

    feat(wails): 添加Wails集成支持
    - 在.gitignore中添加wailsjs目录
    - 更新tsconfig.json以支持Wails类型和路径
    - 添加vite-env.d.ts中的window.platform类型声明
    - 在main.ts中初始化Wails环境
    - 配置vite以解析Wails相关路径

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,6 +3,14 @@ import App from './App.vue'
 import './style.less';
 import { clickOutside } from './utils/directives'
 import { initializeStoreFromCache } from './store'
+import { Environment } from '@wailsjs/runtime/runtime'
+
+window.addEventListener('load', async () => {
+  const env = await Environment()
+  window.platform = env.platform
+  console.log('env: ', env)
+})
+
 
 const app = createApp(App)
 
@@ -10,3 +18,4 @@ app.directive('clickOutside', clickOutside)
 
 initializeStoreFromCache()
 app.mount('#app')
+
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -13,13 +13,15 @@ export default defineConfig({
     __VUE_PROD_DEVTOOLS__: false,
     __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
   },
+  root:'.',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
       '@/components': resolve(__dirname, 'src/components'),
       '@/pages': resolve(__dirname, 'src/pages'),
       '@/utils': resolve(__dirname, 'src/utils'),
-      '@/store': resolve(__dirname, 'src/store')
+      '@/store': resolve(__dirname, 'src/store'),
+      "@wailsjs": resolve(__dirname, "wailsjs"),
     }
   }
 })
